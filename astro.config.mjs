ac16fa6daf01d873dda4d3c824f2c3a2ec1bb95a
mheredia1016 <michael.d.heredia@gmail.com>
2026-06-25 23:08:05 -0500

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: 'static',
-  adapter: netlify()
+  output: 'static'
 });
