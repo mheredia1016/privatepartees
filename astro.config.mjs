5ec6653d54a7900b67909e660cf2321b1971774f
mheredia1016 <michael.d.heredia@gmail.com>
2026-07-09 20:07:25 -0500

    Add Astro sitemap support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  output: 'static'
+  site: 'https://privatepartees.com',
+  output: 'static',
+
+  integrations: [
+    sitemap()
+  ]
 });
