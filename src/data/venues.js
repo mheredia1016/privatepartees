d60d780cd165a93d1e19bc365e8079fef3f7289f
mheredia1016 <michael.d.heredia@gmail.com>
2026-07-11 01:08:37 -0500

    updates

--- src/data/venues.js
+++ src/data/venues.js
@@ -86,15 +86,11 @@ map: 'https://www.google.com/maps?q=2366+N+Neva+Ave+Chicago+IL+60707&output=embe
     style: 'Classic Banquet Hall',
     address: '3140 N 77th Ave, Elmwood Park, IL 60707',
 map: 'https://www.google.com/maps?q=3140+N+77th+Ave+Elmwood+Park+IL+60707&output=embed',
-    gallery: ['/images/venues/club-elmwood/club-elmwood01.png', '/images/venues/club-elmwood/club-elmwood03.png', '/images/venues/club-elmwood/club-elmwood04.png', '/images/venues/club-elmwood/club-elmwood05.png', '/images/venues/club-elmwood/club-elmwood06.png'],
+    gallery: ['/images/venues/club-elmwood/club-elmwood01.png', '/images/venues/club-elmwood/club-elmwood02.png','/images/venues/club-elmwood/club-elmwood03.png', '/images/venues/club-elmwood/club-elmwood04.png', '/images/venues/club-elmwood/club-elmwood05.png', '/images/venues/club-elmwood/club-elmwood06.png'],
     flyers: [
       {
         title: '',
         image: '/images/flyers/club-elmwood/club-elmwood1.png',
-      },
-      {
-        title: '',
-        image: '/images/flyers/club-elmwood/club-elmwood2.png',
       },
       {
         title: '',
